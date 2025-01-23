cb2206a4a601757856c581c7d985d0c206e80cac
Andy <andy@hiii.com.tw>
2025-01-23 15:27:05 +0800

    fix npmignore
    fix npmignore

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,29 +1,27 @@
-import { awscdk } from 'projen';
+import { awscdk } from "projen";
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'hiiiwebadmin',
-  authorAddress: 'webadmin@hiii.com.tw',
-  cdkVersion: '2.176.0',
-  defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.7.0',
-  name: 'hiii-aws-cdk-utils-v2',
+  author: "hiiiwebadmin",
+  authorAddress: "webadmin@hiii.com.tw",
+  cdkVersion: "2.176.0",
+  defaultReleaseBranch: "main",
+  jsiiVersion: "~5.7.0",
+  name: "hiii-aws-cdk-utils-v2",
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/hiiiwebadmin/hiii-aws-cdk-utils-v2.git',
-  description: 'Aws CDK Construct Util Designed For Hiii.',
-  deps: [
-    'aws-cdk-lib',
-    'constructs'
-  ],
-  peerDeps: [
-    'aws-cdk-lib',
-    'constructs'
-  ],
-  devDeps: [
-    'aws-cdk-lib',
-    'constructs'
-  ],
-  keywords: [
-    'cdk',
-    'hiii',
+  repositoryUrl: "https://github.com/hiiiwebadmin/hiii-aws-cdk-utils-v2.git",
+  description: "Aws CDK Construct Util Designed For Hiii.",
+  deps: ["aws-cdk-lib", "constructs"],
+  peerDeps: ["aws-cdk-lib", "constructs"],
+  devDeps: ["aws-cdk-lib", "constructs"],
+  keywords: ["cdk", "hiii"],
+  npmignore: [
+    "cdk.out",
+    "cdk.context.json",
+    "yarn-error.log",
+    "dep.endabot.yml",
+    "website/public",
+    "images",
+    "docs",
+    "website",
   ],
 
   // deps: [],                /* Runtime dependencies of this module. */
@@ -31,7 +29,12 @@ const project = new awscdk.AwsCdkConstructLibrary({
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
-const common_exclude = ['cdk.out', 'cdk.context.json', 'yarn-error.log', 'dep，endabot.yml', 'website/public'];
-// project.npmignore.exclude(...common_exclude, 'images', 'docs', 'website');
+const common_exclude = [
+  "cdk.out",
+  "cdk.context.json",
+  "yarn-error.log",
+  "dep.endabot.yml",
+  "website/public",
+];
 project.gitignore.exclude(...common_exclude);
 project.synth();
